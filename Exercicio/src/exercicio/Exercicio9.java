d79ee569dbfa567b93590b95794c35f4c038bf83
lucassecundes <lssecundes@gmail.com>
2017-04-27 21:21:58 -0300

    Update Exercicio9.java

--- Exercicio/src/exercicio/Exercicio9.java
+++ Exercicio/src/exercicio/Exercicio9.java
@@ -1,25 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exercicio;
 
 import java.util.Scanner;
 
-/**
- *
- * @author aluno
- */
+
 public class Exercicio9 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite o numero da matriz: ");
+        System.out.println("Qual é o numero da matriz: ");
         int matriz = sc.nextInt();
 
         for (int i = 0; i < matriz + 1; i++) {
