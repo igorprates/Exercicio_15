23fb4f13ca5f90bef3592eb17e3ec17d87f39efd
lucassecundes <lssecundes@gmail.com>
2017-04-27 21:24:29 -0300

    Update Exercicio12.java

--- Exercicio/src/exercicio/Exercicio12.java
+++ Exercicio/src/exercicio/Exercicio12.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exercicio;
 
 import java.util.Scanner;
 
-/**
- *
- * @author aluno
- */
+
 public class Exercicio12 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
           float hipotenusa ;
         float rhipotenusa ;
