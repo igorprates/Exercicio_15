cf34b18422fce932b74668cbde354d8139c70ec9
lucassecundes <lssecundes@gmail.com>
2017-04-27 21:24:07 -0300

    Update Exercicio11.java

--- Exercicio/src/exercicio/Exercicio11.java
+++ Exercicio/src/exercicio/Exercicio11.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exercicio;
 
-/**
- *
- * @author aluno
- */
+
 public class Exercicio11 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-         String cidade = "Campo Grande";
+         String cidade = "São Paulo";
         
         System.out.println("O nome da cidade que eu nasci é : " + cidade);
     }
