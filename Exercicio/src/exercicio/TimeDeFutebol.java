6235a916aacbc32e15cf47d5618cb4ac1622fc75
lucassecundes <lssecundes@gmail.com>
2017-04-27 21:27:24 -0300

    Update TimeDeFutebol.java

--- Exercicio/src/exercicio/TimeDeFutebol.java
+++ Exercicio/src/exercicio/TimeDeFutebol.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exercicio;
 
-/**
- *
- * @author aluno
- */
+
 public class TimeDeFutebol {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        ObjetoTimeDeFutebol time1 = new ObjetoTimeDeFutebol("São Paulo FC", "Junior", "Rogerio Ceni", "3 Mundiais");
+        ObjetoTimeDeFutebol time1 = new ObjetoTimeDeFutebol("Brasil", "Titi", "Dida", "5 Copas");
 
 		System.out.println(time1);
     }
