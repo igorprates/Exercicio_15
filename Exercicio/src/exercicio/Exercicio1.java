d27c8cd7fd0ffcf21ead6568380521d697e5144d
lucassecundes <lssecundes@gmail.com>
2017-04-27 21:16:23 -0300

    Update Exercicio1.java

--- Exercicio/src/exercicio/Exercicio1.java
+++ Exercicio/src/exercicio/Exercicio1.java
@@ -1,53 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exercicio;
 
 import java.util.Scanner;
 
-/**
- *
- * @author aluno
- */
 public class Exercicio1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite sua Idade: ");
+        System.out.println("Qual é sua Idade: ");
         int idade = sc.nextInt();
         
-        System.out.println("Digite Seu nome: ");
+        System.out.println("Qual é Seu nome: ");
         String nome = sc.next();
         
-        System.out.println("Digite seu peso: ");
+        System.out.println("Qual é seu peso: ");
         float peso = sc.nextFloat();
         
-        System.out.println("Digite seu tipo sanguineo (Sem o + ou -) : ");
+        System.out.println("Qual é o seu tipo sanguineo (Sem o + ou -) : ");
         String tiposangue = sc.next();
         
-        System.out.println("Digite sua data de aniversário: ");
+        System.out.println("Qual é sua data de aniversário: ");
         String data = sc.next();
         
-        System.out.println("Digite o numero do seu sapato: ");
+        System.out.println("Qual é o numero do seu sapato: ");
         int tamsapato = sc.nextInt();
         
         System.out.println("Casado? (S/N)");
         String casado = sc.next();
         
-        System.out.println("-----------------------FICHA TÉCNICA-----------------------");
-        System.out.println("Nome: " + nome);
-        System.out.println("Idade: " + idade);
+       
+        System.out.println("Seu nome: " + nome);
+        
+        System.out.println("Sua idade: " + idade);
+        
         System.out.println("Data de aniversário: " + data);
+        
         System.out.println("Tipo sanguíneo : " + tiposangue);
+        
         System.out.println("Peso: " + peso + " KG");
+        
         System.out.println("Numero sapato:" + tamsapato);
+        
         System.out.println("Casado: " + casado);
-        System.out.println("-----------------------FIM FICHA TÉCNICA-----------------------");
+        
         
         
     }
