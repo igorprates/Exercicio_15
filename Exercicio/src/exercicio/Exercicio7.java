1ef75ab858ac3ba66ad8aa4fdca30100dc4366c3
lucassecundes <lssecundes@gmail.com>
2017-04-27 21:21:13 -0300

    Update Exercicio7.java

--- Exercicio/src/exercicio/Exercicio7.java
+++ Exercicio/src/exercicio/Exercicio7.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exercicio;
 
 import java.util.Scanner;
 
-/**
- *
- * @author aluno
- */
+
 public class Exercicio7 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         float nota ;
@@ -24,7 +15,7 @@ public class Exercicio7 {
         
         
         do{
-            System.out.println("Informe a nota: ");
+            System.out.println("Qual é a nota: ");
             
             nota = sc.nextFloat();
         
