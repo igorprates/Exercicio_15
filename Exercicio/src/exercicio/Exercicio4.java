ba75fc8aa45f9981065ddfa0c3acbf73dc4033b2
lucassecundes <lssecundes@gmail.com>
2017-04-27 21:18:31 -0300

    Update Exercicio4.java

--- Exercicio/src/exercicio/Exercicio4.java
+++ Exercicio/src/exercicio/Exercicio4.java
@@ -1,32 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exercicio;
 
 import java.util.Scanner;
 
-/**
- *
- * @author aluno
- */
+
 public class Exercicio4 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Informe um numero: ");
+        System.out.println("Digite um numero: ");
         double numero = sc.nextDouble();
         
         if (numero % 2 == 0){
-            System.out.println("Seu número é par !!!");
+            System.out.println("O seu número é par !");
         }
         
         else {
-            System.out.println("Seu número é impar !!!");
+            System.out.println("O seu número é impar !");
     }
     
 }
