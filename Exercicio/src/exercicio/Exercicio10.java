737942f0e7765dc8cbd80a30be0e3a86fe62991f
lucassecundes <lssecundes@gmail.com>
2017-04-27 21:22:31 -0300

    Update Exercicio10.java

--- Exercicio/src/exercicio/Exercicio10.java
+++ Exercicio/src/exercicio/Exercicio10.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exercicio;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author aluno
- */
+
 public class Exercicio10 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         Object[] cityObject = new Object[5];
         String userCity;
